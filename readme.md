26bb7759d97a8aecc094646d933989c8f35e90d3
Ben <nicole@Nicoles-MBP.home>
2022-07-20 13:01:59 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+<h1 align="center">Suitcase - By Ben Rothman
+	<img src="https://img.shields.io/static/v1?ConciergeWPVersion=&message=v1.0.0&label=&color=999&style=flat-square">
+</h1>
+
+<h4 align="center">A program to run to migrate to a new computer by running it on the old computer before getting rid of it to automate the copying of your files and settings into a suitcase directory (pack). Then, you copy the suitcase directory onto the new computer and you run it to unpack the suitcase directory to copy all of your old files and settings onto the new computer with the file and folder structure preserved (unpack).</h4>
+
+<p align="center">
+		<img src="https://img.shields.io/badge/python-v3.7-blue" alt="Python Version">
+</p>
+
+
+## Usage
+Create a directions txt file in the same directory as the main file with one instruction per line (either shell commands to execute starting with a "$", or manual commands that cannot be executed by the shell ie: take a cell phone picture of X).  If the instruction is too complex for an instruction line and cannot be broken into nultiple lines (for instance, download a program and run the installer and enter the registration key) you may start a line with "manual:", which will print out the entered line as a written prompt that you can do and Suitcase will not move to the next command until you press 'Enter'.
+
+## How To Use
+First you need to pack your old computer into the suitcase.  Clone the Suitcase repository from Github, then create the packing instructions text file in the same directory as the python file. You can then run the program with the following command to pack your old computer:
+
+`python3 suitcase.py -p packing_filename.txt`
+
+Once you have packed your old computer into the suitcase, you need to unpack the suitcase on your new computer to preserve the files and settings.  Copy the 'Suitcase' directory onto removable media, copy that directory from the removable media onto the new computer and run suitcase again, but this time with the argument shown below to unpack the suitcase:
+
+`python3 suitcase.py -u unpacking_filename.txt`
+
+Enjoy!
---
+++ suitcase.py
@@ -0,0 +1,133 @@
+# Suitcase by Ben Rothman
+# python3 suitcase.py -<u|p|h> instructions_file.txt
+import os
+import re
+import sys
+import os.path
+from time import sleep
+import subprocess
+from datetime import datetime
+
+# define variables
+today = datetime.now()
+suitcase_path = os.path.dirname(os.path.abspath(__file__)) + "/suitcase/"
+raw_directions = []
+directions = []
+options = sys.argv[1:]
+
+# define color printing variables
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+OKCYAN = '\033[36m'
+YELLOW = '\033[33m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+"""Interperets and executes a command"""
+def step(line):
+    if (line.startswith('##')): ## line is a comment
+        my_print( UNDERLINE + line[3 :] + ENDC)
+        # Skip line
+    elif (current_line.startswith('SKIP ')): # Skip the line
+        print(FAIL + current_line[5:] + ' SKIPPED' + ENDC)
+        sleep(1.0)
+    elif (current_line.startswith('manual: ')):  # manual line (line calls for user execution based on written cue)
+        my_print(YELLOW + BOLD + 'manual: ' + ENDC + line[8:] + ENDC)
+        nun = input('Press Enter when complete.')
+        print('')
+    elif (current_line.startswith('$ ')): # execute shell command
+        my_print(UNDERLINE + 'Execute: ' + line[2:] + ENDC)
+        my_print(OKCYAN + '$ ' + line[2:] + ENDC)
+        ## create subprocess for the command
+        proc = subprocess.Popen(line[2:], stdout=subprocess.PIPE, shell=True)
+        (output, err) = proc.communicate()
+        ## Wait for date to terminate. Get return returncode ##
+        p_status = proc.wait()
+        if (p_status == 0):
+            print(OKGREEN + 'Execution Successful ' + BOLD + '' + ENDC)
+        else:
+            print(FAIL + "Return: " + str(p_status) + "\n" + str(err) + ENDC)
+
+# define 'my_print()
+def my_print(words):
+    for char in words:
+        sleep(0.05)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+    sys.stdout.write("\n")
+
+
+
+
+
+
+###### PROGRAM ######
+# Clear the terminal
+my_print(chr(27) + "[2J")
+my_print('Welcome to Suitcase ' + str(today))
+
+if (not os.path.exists('suitcase')):
+    os.system('mkdir suitcase')
+
+
+option = options[0]
+instructions_file = options[1]
+
+# Behave based on arguments
+if (option == '-h' or option == '-help'): # help
+    print('-----')
+    print(UNDERLINE + '-h | help' + ENDC)
+    my_print('This option my_prints out the different options that can be used to run the suitcase.')
+    my_print('')
+    my_print(UNDERLINE + '-p | pack' + ENDC)
+    my_print('This option tells the script to pack the suitcase based on the packing list.')
+    my_print('')
+    my_print(UNDERLINE + '-u | unpack' + ENDC)
+    my_print('This option unpacks the suitcase based on the unpacking list.')
+    my_print('')
+    print('-----')
+elif (option == '-p'): # pack
+    # read each line from the packing file put them in array $raw_directions
+    with open(instructions_file) as my_file:
+        for line in my_file:
+            raw_directions.append(line)
+
+    # Replace !!/ with suitcase directory in all lines of array $raw_directions and push them to array $directions
+    for i in range(len(raw_directions)):
+        directions.append(re.sub('!!\/', suitcase_path, raw_directions[i]))
+
+    # my_print Header
+    my_print(HEADER + UNDERLINE + 'PACKING LIST:' + ENDC + "\n")
+
+    # Execute each line of array $directions
+    for current_line in directions:
+        step(current_line)
+
+    my_print('Packing Complete.')
+elif ('-u' in option): #unpack
+    # Clear the terminal
+    my_print(chr(27) + "[2J")
+
+    # my_print Header
+    my_print(HEADER + UNDERLINE + 'UNPACKING LIST:' + ENDC + "\n")
+
+    # read each line from the unpacking file put them in array $raw_directions
+    with open(instructions_file) as my_file:
+        for line in my_file:
+            raw_directions.append(line)
+
+    # Replace !!/ with suitcase directory in all lines of array $raw_directions and push them to array $directions
+    for i in range(len(raw_directions)):
+        directions.append(re.sub('!!\/', suitcase_path, raw_directions[i]))
+
+    # Execute each line of array $directions
+    for current_line in directions:
+        step(current_line)
+
+    my_print('Unpacking Complete.')
+
+##### END PROGRAM #####
